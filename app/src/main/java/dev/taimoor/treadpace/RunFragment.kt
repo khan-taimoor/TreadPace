e1a4c84639f98a332df6b850f49e3d57111d9e78
Taimoor Khan <taimoor.khan@uconn.edu>
2019-12-08 21:34:48 -0500

    show map on run screen

--- app/src/main/java/dev/taimoor/treadpace/RunFragment.kt
+++ app/src/main/java/dev/taimoor/treadpace/RunFragment.kt
@@ -7,8 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.OnMapReadyCallback
+import kotlinx.android.synthetic.main.run_layout.*
 
-class RunFragment : Fragment() {
+class RunFragment : Fragment(), OnMapReadyCallback {
+
+
+    var map: GoogleMap? = null
+    var mapView: MapView? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,7 +28,25 @@ class RunFragment : Fragment() {
         setHasOptionsMenu(true)
 
         return inflater.inflate(R.layout.run_layout, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView = map_view
+
+        if(mapView != null){
+            mapView?.onCreate(null)
+            mapView?.onResume()
+            mapView?.getMapAsync(this)
+        }
+    }
+
+
+    override fun onMapReady(p0: GoogleMap?) {
+        MapsInitializer.initialize(context)
+        this.map = map
+        map?.isMyLocationEnabled = true
     }
 
 }
