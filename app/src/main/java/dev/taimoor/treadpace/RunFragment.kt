3352f57d3536260203b5e8e7af9ed5b12ed61c53
Taimoor Khan <taimoor.khan@uconn.edu>
2019-12-10 22:17:47 -0500

    move camera and zoom to street level when opening run screen

--- app/src/main/java/dev/taimoor/treadpace/RunFragment.kt
+++ app/src/main/java/dev/taimoor/treadpace/RunFragment.kt
@@ -1,23 +1,31 @@
 package dev.taimoor.treadpace
 
+import android.content.Context
+import android.location.Location
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.MapsInitializer
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.run_layout.*
 
 class RunFragment : Fragment(), OnMapReadyCallback {
 
 
-    var map: GoogleMap? = null
-    var mapView: MapView? = null
+    private var map: GoogleMap? = null
+    private var mapView: MapView? = null
+    private var uiSettings : UiSettings? = null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +36,13 @@ class RunFragment : Fragment(), OnMapReadyCallback {
         setHasOptionsMenu(true)
 
         return inflater.inflate(R.layout.run_layout, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity?.applicationContext as Context)
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -43,10 +58,23 @@ class RunFragment : Fragment(), OnMapReadyCallback {
     }
 
 
-    override fun onMapReady(p0: GoogleMap?) {
+    override fun onMapReady(map: GoogleMap?) {
         MapsInitializer.initialize(context)
         this.map = map
         map?.isMyLocationEnabled = true
+
+        uiSettings = map?.uiSettings
+        uiSettings?.isRotateGesturesEnabled = false
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if(location != null){
+                Toast.makeText(context, "yea", Toast.LENGTH_SHORT).show()
+                this.map?.moveCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.builder().zoom(15.0f).target(LatLng(location.latitude, location.longitude)).build()))
+            }
+        }
+
+
+
     }
 
 }
