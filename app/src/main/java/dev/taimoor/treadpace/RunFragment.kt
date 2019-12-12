df825d9d125534906ad8fdfe7b6e1eb675b8bd89
Taimoor Khan <taimoor.khan@uconn.edu>
2019-12-12 00:11:05 -0500

    followed tutorial on change location settings and made location request

--- app/src/main/java/dev/taimoor/treadpace/RunFragment.kt
+++ app/src/main/java/dev/taimoor/treadpace/RunFragment.kt
@@ -1,9 +1,11 @@
 package dev.taimoor.treadpace
 
 import android.content.Context
+import android.content.IntentSender
 import android.location.Location
 import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +13,12 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.run_layout.*
 
 class RunFragment : Fragment(), OnMapReadyCallback {
@@ -24,7 +27,7 @@ class RunFragment : Fragment(), OnMapReadyCallback {
     private var map: GoogleMap? = null
     private var mapView: MapView? = null
     private var uiSettings : UiSettings? = null
-    private var currentLocation : Location = null
+    private var currentLocation : Location? = null
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
 
@@ -72,12 +75,78 @@ class RunFragment : Fragment(), OnMapReadyCallback {
                 Toast.makeText(context, "yea", Toast.LENGTH_SHORT).show()
                 //this.map?.moveCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.builder().tilt(45.0f).zoom(15.0f).target(LatLng(location.latitude, location.longitude)).build()))
                 currentLocation = location
-                this.map?.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(currentLocation.latitude, currentLocation.longitude), 15.0f))
+                this.map?.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 15.0f))
+            }
+        }
+
+        createLocationRequest()
+    }
+
+
+    // from Google
+    fun createLocationRequest() {
+        val locationRequest = LocationRequest.create()?.apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+
+
+        var builder : LocationSettingsRequest.Builder? = null
+        var client : SettingsClient? = null
+
+        locationRequest?.let {
+            builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        }
+
+        activity?.let {
+            client = LocationServices.getSettingsClient(it.applicationContext)
+        }
+
+        var task: Task<LocationSettingsResponse>? = null
+        Log.d("In run fragment", "Making sure I get here.")
+
+        safeLet(builder, client) { b, c ->
+            task = c.checkLocationSettings(b.build())
+            Toast.makeText(context, "CHECKING LOCATION SETTINGS@", Toast.LENGTH_LONG).show()
+
+        }
+
+
+        task?.addOnSuccessListener { locationSettingsResponse ->
+            Toast.makeText(context, "location settings request MADE", Toast.LENGTH_LONG).show()
+
+        }
+
+        task?.addOnFailureListener { exception ->
+
+            Toast.makeText(context, "location settings request FAILED", Toast.LENGTH_LONG).show()
+
+            if (exception is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(activity, 1)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
             }
         }
 
 
 
     }
+
+    // https://www.reddit.com/r/androiddev/comments/6nuxb8/null_checking_multiple_vars_in_kotlin/ reddit user gonemad16
+    // take in 2 variables, p1 and p2, and a lambda
+    // if both vars aren't none, execute the block
+    fun <T1: Any, T2: Any, R: Any> safeLet(p1: T1?, p2: T2?, block: (T1, T2)->R?): R? {
+        return if (p1 != null && p2 != null) block(p1, p2) else null
+    }
+
+
 
 }
